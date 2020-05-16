356d96db0a17f06d50d4e2d6bb68d7ee8ab835ea
Marie Hrabovska <marie.hrabovska@gmail.com>
2020-05-16 21:39:51 +0200

    Vytvorena fce updateTasks

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 'use strict';
 
+let tasks = [];
+let todo__tasksElm = document.querySelector('.todo__tasks');
+
+const updateTasks = () => {
+  todo__tasksElm.innerHTML = '';
+  for (let i = 0; i < tasks.length; i++) {
+    todo__tasksElm.innerHTML +=
+      `<div class="task"> ${tasks[i]} </div>`
+  }
+};
+
+
+
 console.log('funguju!');
+
+//const newTaskElm = document.querySelector('#new-task');
+//const btnElm = document.querySelector('.btn-add');
+
